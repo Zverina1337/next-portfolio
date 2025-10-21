1b7b70ec5a008ef49a4d42b2183167c108a38629
Zverina1337 <danii.zvorugin@gmail.com>
2025-10-21 15:17:53 +0700

    fix: about-page fix import

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,6 @@
 
 import { useRef } from 'react'
 
-import SiteNav from '@/components/navigation/Navigation.client'
-import CinematicIntro from '@/app/(home)/components/BlockIntro'
-import AboutOrbit from '@/app/(home)/components/about/AboutOrbit'
-import ProjectsBlock from '@/app/(home)/components/ProjectsBlock'
-import ContactBlock from '@/app/(home)/components/ContactBlock'
-
 export default function HeroPage() {
   const wrapperRef = useRef<HTMLDivElement>(null)
   const contentRef = useRef<HTMLDivElement>(null)
@@ -15,11 +9,7 @@ export default function HeroPage() {
   return (
     <div ref={wrapperRef} id="smooth-wrapper" className="overflow-hidden w-full">
       <div ref={contentRef} id="smooth-content">
-        <SiteNav />
-        <CinematicIntro />
-        <AboutOrbit />
-        <ProjectsBlock />
-        <ContactBlock email='danii.zvorugin@gmail.com' />
+        About page
       </div>
     </div>
   )
